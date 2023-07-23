055ee1b22134055b761153d9b7dc25e554b5cedb
kys0411 <32180846@dankook.ac.kr>
2023-07-23 12:21:08 +0900

    feat: 댓글 삭제 기능 추가

--- src/main/java/programmers/noticeBoard/service/CommentService.java
+++ src/main/java/programmers/noticeBoard/service/CommentService.java
@@ -8,6 +8,7 @@ import programmers.noticeBoard.domain.Member;
 import programmers.noticeBoard.domain.Post;
 import programmers.noticeBoard.dto.CommentDto;
 import programmers.noticeBoard.repository.CommentRepository;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +24,13 @@ public class CommentService {
         Comment comment = new Comment(request.getContent(), member, post);
         post.addComment(comment);
         commentRepository.save(comment);
+    }
+
+    public void deleteComment(Long postId, Long commentId) {
+        Post post = postService.findPostById(postId);
+        Comment comment = commentRepository.findById(commentId)
+                        .orElseThrow(() -> new NoSuchElementException("댓글이 없습니다"));
+        post.deleteComment(comment);
+        commentRepository.deleteById(commentId);
     }
 }
