f8c91d840d859127534a809edd24231fdabe34e5
kys0411 <32180846@dankook.ac.kr>
2023-07-23 11:55:37 +0900

    add: member request & response dto 추가

---
+++ src/main/java/programmers/noticeBoard/dto/MemberDto.java
@@ -0,0 +1,53 @@
+package programmers.noticeBoard.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+public class MemberDto {
+
+    @Getter @Setter
+    public static class Request {
+
+        private String username;
+
+        private String password;
+
+        private String profileImage;
+
+        private String selfIntro;
+    }
+
+    @Getter
+    public static class Response {
+
+        private Long id;
+
+        private String username;
+
+        private String profileImage;
+
+        private String selfIntro;
+
+        private LocalDateTime createdAt;
+
+        private LocalDateTime updatedAt;
+
+        @Builder
+        public Response(Long id,
+                        String username,
+                        String profileImage,
+                        String selfIntro,
+                        LocalDateTime createdAt,
+                        LocalDateTime updatedAt) {
+            this.id = id;
+            this.username = username;
+            this.profileImage = profileImage;
+            this.selfIntro = selfIntro;
+            this.createdAt = createdAt;
+            this.updatedAt = updatedAt;
+        }
+    }
+}
