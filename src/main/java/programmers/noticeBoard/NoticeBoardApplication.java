156bc844ea95f3b29477ff9c8f128a30a00fd7e3
kys0411 <32180846@dankook.ac.kr>
2023-07-23 12:06:23 +0900

    add: enableJpaAuditing 어노테이션 추가

--- src/main/java/programmers/noticeBoard/NoticeBoardApplication.java
+++ src/main/java/programmers/noticeBoard/NoticeBoardApplication.java
@@ -2,8 +2,10 @@ package programmers.noticeBoard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NoticeBoardApplication {
 
 	public static void main(String[] args) {
